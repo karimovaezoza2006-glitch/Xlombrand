4a6814be3fb7d58ddcdadd442932543fcc8ed29b
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-09-21 17:18:09 +0500

    methods

--- main.js
+++ main.js
@@ -1,72 +1,80 @@
-
-// const  dasturchi={
-//     ism:"ezoza",
-//     yosh:19,
-//     salomlashish: function(){
-//         console.log("Salom! Mening ismim"+ this.ism);
-
-//     }
-// }
-// dasturchi.salomlashish();
-
-
-let arr_number =[1,2,3,4,5,6];
-let arr = [true, "string", 22, true, "hello "];
-// let new_arr = arr_number.map((value) => {
-//     return value * 2 ;
-// });
-// console.log(new_arr);
-
-let person_arr = [
-    {
-        id:1,
-        name:"Olim",
-        surname:"Olimov",
-        job:"Dehqon", 
-        price:300000,
-    },
-    {
-       id:2,
-       name:"Zuxra",
-       surname:"Umaraliyev",
-       job:"Tikuvchi" ,
-       price:232332,
-    }
-];
-
-person_arr.forEach((value) =>{
-    value.surname = "Yunusov";
-});
-console.log(person_arr);
-// let obj ={
-//     surname:"surname",
-// };
-
-
-// let new_arr = arr_number.map((value) => {
-//     return value * 2 ;
-// });
-// console.log(new_arr);
-
-
-//  Array.prototype.myMap = function
-
-// Array.prototype.myForEach = function (cb){
-//     for ( let i=0; i<this.length; i++){
-//         cb(this[i], i,this);
-//     }
-// };
-// arr.myForEach((value, edx, arr) =>{
-//     console.log(value, edx, arr);
-// });
-
-
-Array.prototype.myMap = function(cb) {
-    let result = [];
-    for (let i = 0; i < this.length; i++) {
-        result.push(cb(this[i], i, this));
-    }
-    return result;
+//1-masala map yordamida ishlash
+const arr = [1, 2, 3, 4, 5];
+Array.prototype.myMap = function(callback) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
 };
+console.log(arr.myMap(x => x * 2)); 
 
-let arr1 = [true, "string", 22, true, "hello "];
+
+
+//2-masala filter methodi yordamida ishlash
+const arr1 = [1, 2, 3, 4, 5];
+Array.prototype.myFilter = function(callback) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+console.log(arr.myFilter(x => x > 2)); 
+
+
+//3-masala some methodi yordamida masalani ishlash
+const arr2 = [1, 2, 3, 4, 5];
+Array.prototype.mySome = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return true;
+    }
+  }
+  return false;
+};
+console.log(arr2.mySome(x => x === 3)); 
+
+
+
+//4-reduce methodi yordamida ishlash
+const arr3 = [1, 2, 3, 4, 5];
+Array.prototype.myReduce = function(callback, initialValue) {
+  let accumulator = initialValue !== undefined ? initialValue : this[0];
+  let startIndex = initialValue !== undefined ? 0 : 1;
+
+  for (let i = startIndex; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
+  return accumulator;
+};
+console.log(arr3.myReduce((a, b) => a + b, 0));
+
+
+
+//5-masala every methodi yordamida
+const arr4 = [1, 2, 3, 4, 5];
+Array.prototype.myEvery = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (!callback(this[i], i, this)) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(arr4.myEvery(x => x > 0));
+
+
+//6-masala 
+const arr5 = [1, 2, 3, 4, 5];
+Array.prototype.myFind = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return this[i];
+    }
+  }
+  return undefined;
+};
+console.log(arr5.myFind(x => x === 4));
